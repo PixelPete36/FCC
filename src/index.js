0680f9b43d84a2ef82a906422a39085548d6570c
PixelPete36 <pixelpete36@gmail.com>
2023-09-12 19:43:36 -0600

    changed Appp to App

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { marked } from "marked";
 
-class Appp extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -98,11 +98,6 @@ const Previewer = (props) => {
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
-    <Appp />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
